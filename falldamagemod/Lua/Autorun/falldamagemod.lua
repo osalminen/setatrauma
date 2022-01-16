3f6878bb5fc80d2b3dab839614f35ce132373a85
Otto Salminen <otto.salminen@lut.fi>
2022-01-16 14:35:19 +0200

    Initial commit

---
+++ falldamagemod/Lua/Autorun/falldamagemod.lua
@@ -0,0 +1,33 @@
+
+local bonkprefab
+for k, v in pairs(AfflictionPrefab.ListArray) do
+    if v.name == "Blunt force trauma" then
+       bonkprefab = v
+       break
+    end
+end
+
+Hook.Add("changeFallDamage", "testFallDamage", function (amount, character, impactpos, velocity)
+    if character.AnimController ~= nil then
+        for _, limb in pairs(character.AnimController.Limbs) do
+            local sqvel = (velocity.Length()) * (velocity.Length())
+            --character.CharacterHealth.ApplyAffliction(limb, bonkprefab.Instantiate(dmg))
+            local checkdist = 30
+            local multiplier = 1
+            if sqvel > 120 then
+                checkdist = 100
+                multiplier = 1
+            end
+            if sqvel >= 150 then
+                checkdist = 300
+                multiplier = 3
+            end
+            local dmg = (sqvel * 3)/(limb.WorldPosition - impactpos).Length() * multiplier
+            if (limb.WorldPosition - impactpos).Length() < checkdist then
+               character.CharacterHealth.ApplyAffliction(limb, bonkprefab.Instantiate(dmg))
+            end
+        end
+    end
+    return 0
+end)
+
