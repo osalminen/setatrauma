70925594e039461a81e2ed8809a1e61e35e2cdef
Otto Salminen <otto.salminen@lut.fi>
2022-01-16 23:09:04 +0200

    Reduce blunt force trauma, see if it makes playing more bearable.

--- falldamagemod/Lua/Autorun/falldamagemod.lua
+++ falldamagemod/Lua/Autorun/falldamagemod.lua
@@ -20,7 +20,7 @@ Hook.Add("changeFallDamage", "testFallDamage", function (amount, character, impa
             end
             if sqvel >= 150 then
                 checkdist = 300
-                multiplier = 3
+                multiplier = 2
             end
             local dmg = (sqvel * 3)/(limb.WorldPosition - impactpos).Length() * multiplier
             if (limb.WorldPosition - impactpos).Length() < checkdist then
