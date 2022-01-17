853f802841859bdfd5139ebcfbab5e508948fb8d
Otto Salminen <otto.salminen@lut.fi>
2022-01-17 22:16:51 +0200

    Fixed tabs, removed debugging

--- falldamagemod/Lua/Autorun/falldamagemod.lua
+++ falldamagemod/Lua/Autorun/falldamagemod.lua
@@ -16,10 +16,10 @@ Hook.Add("changeFallDamage", "testFallDamage", function (amount, character, impa
             local checkdist = 30 --Ragdoll to wall type of fall
             local multiplier = 0.5
 			
-			if sqvel > 60 then -- 1 floor drop
-				checkdist = 50
-				multiplier = 0.7
-			end
+            if sqvel > 60 then -- 1 floor drop
+                checkdist = 50
+                multiplier = 0.7
+            end
 			
             if sqvel > 120 then -- 2 floor drop = very fucked
                 checkdist = 100
@@ -33,9 +33,9 @@ Hook.Add("changeFallDamage", "testFallDamage", function (amount, character, impa
 			
             local dmg = (sqvel * 3)/(limb.WorldPosition - impactpos).Length() * multiplier
             if (limb.WorldPosition - impactpos).Length() < checkdist then
-               character.CharacterHealth.ApplyAffliction(limb, bonkprefab.Instantiate(dmg))
-			   Game.SendMessage(sqvel, ChatMessageType.Server)
-			   Game.SendMessage(dmg, ChatMessageType.Server)
+                character.CharacterHealth.ApplyAffliction(limb, bonkprefab.Instantiate(dmg))
+                --Game.SendMessage(sqvel, ChatMessageType.Server) --For debugging purposes
+                --Game.SendMessage(dmg, ChatMessageType.Server) --For debugging purposes
             end
         end
     end
