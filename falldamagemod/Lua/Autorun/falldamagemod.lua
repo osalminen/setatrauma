c117ac55ac3eca2a9e89adab1b123a528ace4074
Otto Salminen <otto.salminen@lut.fi>
2022-01-17 22:13:41 +0200

    Forgot to add the edits ":D"

--- falldamagemod/Lua/Autorun/falldamagemod.lua
+++ falldamagemod/Lua/Autorun/falldamagemod.lua
@@ -12,19 +12,30 @@ Hook.Add("changeFallDamage", "testFallDamage", function (amount, character, impa
         for _, limb in pairs(character.AnimController.Limbs) do
             local sqvel = (velocity.Length()) * (velocity.Length())
             --character.CharacterHealth.ApplyAffliction(limb, bonkprefab.Instantiate(dmg))
-            local checkdist = 30
-            local multiplier = 1
-            if sqvel > 120 then
+			
+            local checkdist = 30 --Ragdoll to wall type of fall
+            local multiplier = 0.5
+			
+			if sqvel > 60 then -- 1 floor drop
+				checkdist = 50
+				multiplier = 0.7
+			end
+			
+            if sqvel > 120 then -- 2 floor drop = very fucked
                 checkdist = 100
-                multiplier = 0.2
+                multiplier = 1
             end
-            if sqvel >= 150 then
+			
+            if sqvel >= 150 then -- 3 floor drop = ded
                 checkdist = 300
-                multiplier = 5
+                multiplier = 2
             end
+			
             local dmg = (sqvel * 3)/(limb.WorldPosition - impactpos).Length() * multiplier
             if (limb.WorldPosition - impactpos).Length() < checkdist then
                character.CharacterHealth.ApplyAffliction(limb, bonkprefab.Instantiate(dmg))
+			   Game.SendMessage(sqvel, ChatMessageType.Server)
+			   Game.SendMessage(dmg, ChatMessageType.Server)
             end
         end
     end
